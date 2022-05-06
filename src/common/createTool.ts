2167dd71ee26d8d2bd0049257fa4f173be85d6f8
milkplane <victorgelign@gmail.com>
2022-05-06 08:43:18 +0300

    Add tool creating

---
+++ src/common/createTool.ts
@@ -0,0 +1,27 @@
+import { CellType } from "./createCell";
+
+type Tool = {
+    size: number;
+    instantHeightIncrease: number;
+    increaseRate: number;
+    brushType: CellType | null;
+}
+
+const createTool = (size: number, instantHeightIncrease: number, increaseRate: number, brushType: CellType | null): Tool => {
+    return {
+        size,
+        instantHeightIncrease,
+        increaseRate,
+        brushType,
+    }
+}
+
+export const createBrush = (size: number, brushType: CellType): Tool => {
+    return createTool(size, 0, 0, brushType);
+}
+
+export const createTerrainTool = (size: number, instantHeightIncrease: number, increaseRate: number): Tool => {
+    return createTool(size, instantHeightIncrease, increaseRate, null);
+}
+
+export default createTool;
