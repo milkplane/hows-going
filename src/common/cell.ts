948678bcc3c26d907d8f168419a3ccc8abdfcfff
milkplane <victorgelign@gmail.com>
2022-05-08 09:58:56 +0300

    Add type restriction for certain heights

--- src/common/cell.ts
+++ src/common/cell.ts
@@ -15,9 +15,23 @@ export const copyCell = (cell: Cell): Cell => {
     };
 }
 
+const TOO_HIGH_FOR_WATER = 0;
+const TOO_HIGH_FOR_BUSH = 0.3;
+const TOO_LOW_FOR_BUSH = -0.1;
+
+const getVerifiedType = (height: number, type: CellType): CellType => {
+    if (type === CellType.Bush && height >= TOO_HIGH_FOR_BUSH ||
+        type === CellType.Bush && height <= TOO_LOW_FOR_BUSH ||
+        type === CellType.Water && height >= TOO_HIGH_FOR_WATER) {
+        return CellType.Ground;
+    }
+
+    return type;
+}
+
 export const createCell = (height: number, type: CellType = CellType.Ground): Cell => {
     return {
         height,
-        type,
+        type: getVerifiedType(height, type),
     };
 }
