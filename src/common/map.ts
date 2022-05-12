13241204d1d874f54d011cdab72948982936f000
milkplane <victorgelign@gmail.com>
2022-05-12 10:27:47 +0300

    Add tool applying to map

--- src/common/map.ts
+++ src/common/map.ts
@@ -34,3 +34,36 @@ const isColumnInMap = (j: number, map: MapData): boolean => {
 export const isInMap = (coords: Coords, map: MapData): boolean => {
     return isRowInMap(coords.i, map) && isColumnInMap(coords.j, map);
 }
+
+const getChangesFromTool = (map: MapData, tool: Tool, coords: Coords): MapChanges => {
+    const canExpandTo = (coords: Coords) => isInMap(coords, map);
+    const areaRoot = expandTree(createTree(coords), tool.size, canExpandTo);
+    const getPressure = (distanceFromCenter: number) => distanceFromCenter * tool.increaseRate;
+    const pressureInfos = arrayFromTree<number>(areaRoot, getPressure);
+    return pressureInfos.map((info) => {
+        const tooledHeight = tool.instantHeightIncrease + info.mappedValue;
+        const cell = changeCellHeight(map[info.coords.i][info.coords.j], tooledHeight);
+
+        if (tool.brushType) {
+            return {
+                coords: info.coords,
+                newCell: changeCellType(cell, tool.brushType),
+            }
+        }
+        return {
+            coords: info.coords,
+            newCell: cell,
+        }
+    })
+}
+
+export const createAppliedToolMap = (map: MapData, tool: Tool, coords: Coords): MapData => {
+    const copiedMap = copyMap(map);
+    const changesFromTool = getChangesFromTool(map, tool, coords);
+
+    changesFromTool.forEach(change => {
+        copiedMap[change.coords.i][change.coords.j] = copyCell(change.newCell);
+    });
+
+    return copiedMap;
+}
