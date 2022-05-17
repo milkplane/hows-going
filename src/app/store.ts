ede2930ff3af9b1e61bc3d27792d5cf26245ece9
milkplane <victorgelign@gmail.com>
2022-05-17 14:28:09 +0300

    Configure store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import findingFlow from '../features/findingFlow/findingFlowSlice';
+import map from '../features/map/mapSlice';
 
 export const store = configureStore({
   reducer: {
+    map,
+    findingFlow,
   },
 });
 
