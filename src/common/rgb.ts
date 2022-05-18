12a1f75c47e2bb745353af3a8595678a3203e551
milkplane <victorgelign@gmail.com>
2022-05-18 12:06:55 +0300

    Change gradient

--- src/common/rgb.ts
+++ src/common/rgb.ts
@@ -16,7 +16,7 @@ const getShiftedPrimaryColor = (color1: number, color2: number, shift: number) =
     return (color2 - color1) * shift + color1;
 }
 
-export const getColorBetween = (color1: RGB, color2: RGB, shift: number) => {
+export const getColorBetween = (color1: RGB, color2: RGB, shift: number): RGB => {
     const redShift = getShiftedPrimaryColor(color1.red, color2.red, shift);
     const greenShift = getShiftedPrimaryColor(color1.green, color2.green, shift);
     const blueShift = getShiftedPrimaryColor(color1.blue, color2.blue, shift);
@@ -28,27 +28,45 @@ export const getColorBetween = (color1: RGB, color2: RGB, shift: number) => {
     )
 }
 
-export type Gradient = {
-    pointColors: RGB[];
+export type GradientPoint = {
+    color: RGB;
+    shift: number;
 }
 
-export const createGradient = (...colors: RGB[]): Gradient => {
+export type Gradient = {
+    pointColors: GradientPoint[];
+}
+
+export const createGradientPoint = (color: RGB, shift: number) => {
+    return {
+        color,
+        shift,
+    }
+}
+
+export const createGradient = (...colors: GradientPoint[]): Gradient => {
     return {
         pointColors: colors,
     }
 }
 
-export const getGradientColor = (gradient: Gradient, shift: number) => {
+export const getGradientColor = (gradient: Gradient, shift: number): RGB => {
     if (gradient.pointColors.length === 1) {
-        return gradient.pointColors[0];
+        return gradient.pointColors[0].color;
     }
 
-    const shiftInGradient = (gradient.pointColors.length - 1) * shift; // -1 Last color shouldnt be taken 
-    const colorIndex = Math.floor(shiftInGradient);
-    const shiftInColor = shiftInGradient - colorIndex;
-    const secondColorIndex = colorIndex + 1;
+    const secondColorIndex = gradient.pointColors.findIndex((colorPoint) => {
+        return colorPoint.shift >= shift;
+    });
 
-    return getColorBetween(gradient.pointColors[colorIndex], gradient.pointColors[secondColorIndex], shiftInColor);
+    if (secondColorIndex === 0) return gradient.pointColors[secondColorIndex].color;
+
+    const firstColorIndex = secondColorIndex - 1;
+    const firstColorPoint = gradient.pointColors[firstColorIndex];
+    const secondColorPoint = gradient.pointColors[secondColorIndex];
+    const shiftInColor = (shift - firstColorPoint.shift) / (secondColorPoint.shift - firstColorPoint.shift );
+
+    return getColorBetween(firstColorPoint.color, secondColorPoint.color, shiftInColor);
 }
 
 export const stringifyColor = (color: RGB) => {
