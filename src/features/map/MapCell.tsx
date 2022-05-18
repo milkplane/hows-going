af5505f033bd1b2f919eb1fa58d880b2142de49b
milkplane <victorgelign@gmail.com>
2022-05-18 12:08:50 +0300

    Add cell coloring

--- src/features/map/MapCell.tsx
+++ src/features/map/MapCell.tsx
@@ -1,19 +1,53 @@
 import { useAppSelector } from "../../app/hooks";
-import { getRoughness } from "../../common/cell";
+import { CellColorGetter, CellType, getCellColor, getRoughness } from "../../common/cell";
 import { createCoords } from "../../common/coords";
 import { getCell } from "../../common/map";
+import styled from "styled-components";
+import { createColor, createGradient, createGradientPoint, stringifyColor } from "../../common/rgb";
 
 type CellCoords = {
     row: number;
     column: number;
 }
 
+const Tile = styled.td`
+    background-color: ${props => props.color};
+    min-width: 50px;
+    height: 50px;
+`
+
+const groundColor = createGradient(
+    createGradientPoint(createColor(155, 118, 83), 0),
+    createGradientPoint(createColor(255, 253, 234), 0.5),
+    createGradientPoint(createColor(126, 113, 107), 0.75),
+    createGradientPoint(createColor(246, 245, 243), 1)
+)
+
+const waterColor = createGradient(
+    createGradientPoint(createColor(155, 118, 83), 0),
+    createGradientPoint(createColor(205, 237, 247), 1)
+)
+
+const bushColor = createGradient(
+    createGradientPoint(createColor(205, 237, 247), 1)
+)
+
+const getTypedColor: CellColorGetter = (type: CellType) => {
+    if (type === CellType.Bush) return bushColor;
+    if (type === CellType.Water) return waterColor;
+    return groundColor;
+}
+
+const primaryPath = createColor(209, 164, 105);
+const primaryVisited = createColor(199, 199, 197);
+
 const MapCell = (props: CellCoords) => {
     const coords = createCoords(props.row, props.column);
+    const cell = useAppSelector(state => getCell(state.map.map, coords));
     const roughness = useAppSelector(state => getRoughness(getCell(state.map.map, coords)));
-    return <td>
-        {roughness}
-    </td>
+    const type = useAppSelector(state => getCell(state.map.map, coords).type)
+    const terrainColor = getCellColor(cell, getTypedColor);
+    return <Tile color={stringifyColor(terrainColor)}/>
 }
 
 export default MapCell;
