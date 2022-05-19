fb18ff7a0c69547fc1ccc0c5b7a3a189de7cdb82
milkplane <victorgelign@gmail.com>
2022-05-19 14:47:49 +0300

    Add game object dragndrop

--- src/features/map/Map.tsx
+++ src/features/map/Map.tsx
@@ -1,13 +1,30 @@
+import { useEffect } from "react";
+import { MouseEvent } from "react";
 import { useState } from "react";
-import { useAppSelector } from "../../app/hooks";
-import { Coords, createCoords } from "../../common/coords";
-import { useTool } from "./hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { areEqualCoords, Coords, createCoords } from "../../common/coords";
+import { useGameObjectDrag, useTool } from "./hooks";
 import MapRow from "./MapRow";
+import { endChanged, startChanged } from "./mapSlice";
 
 const Map = (props: any) => {
     const height = useAppSelector(state => state.map.size.height);
     const [hoveredCell, setHoveredCell] = useState<Coords>(createCoords(3, 3));
-    const onMapPressed = useTool(hoveredCell);
+    const handleToolPressed = useTool(hoveredCell);
+    const start = useAppSelector(state => state.map.start);
+    const end = useAppSelector(state => state.map.end);
+    const handleStartPressed = useGameObjectDrag(start, hoveredCell, startChanged);
+    const handleEngChanging = useGameObjectDrag(end, hoveredCell, endChanged);
+
+    const onMapPressed = (event: MouseEvent<HTMLTableSectionElement>) => {
+        if (areEqualCoords(hoveredCell, start)) {
+            handleStartPressed(event);
+        } else if (areEqualCoords(hoveredCell, end)) {
+            handleEngChanging(event);
+        } else {
+            handleToolPressed(event);
+        }
+    }
 
     const rows = [];
 
--- src/features/map/hooks.ts
+++ src/features/map/hooks.ts
@@ -1,8 +1,9 @@
+import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 import { useState } from "react";
 import { MouseEvent } from "react";
 import { useEffect } from "react";
 import { useAppDispatch } from "../../app/hooks";
-import { Coords } from "../../common/coords";
+import { areEqualCoords, Coords } from "../../common/coords";
 import { toolApplied } from "./mapSlice";
 
 export const useTool = (coords: Coords) => {
@@ -19,16 +20,42 @@ export const useTool = (coords: Coords) => {
         if (!isMapPressed) return;
 
         dispatch(toolApplied(coords));
-        
+
         const timer = setInterval(() => {
             dispatch(toolApplied(coords));
         }, 1000 / 2);
 
-        return  () => {
+        return () => {
             clearInterval(timer);
         }
-        
+
     }, [isMapPressed, coords])
 
     return onMapPressed;
+}
+
+
+export const useGameObjectDrag = (objectPosition: Coords, hoveredCell: Coords, objectChanged: ActionCreatorWithPayload<Coords, string>) => {
+    const dipatch = useAppDispatch();
+    const [isObjectChanging, setIsObjectChanging] = useState<boolean>(false);
+
+    const onGameObjectPressed = (event: MouseEvent<HTMLTableSectionElement>) => {
+        event.preventDefault();
+
+        setIsObjectChanging(true);
+        document.addEventListener('mouseup', () => setIsObjectChanging(false));
+    }
+
+    useEffect(() => {
+        if (!isObjectChanging) return;
+
+        dipatch(objectChanged(hoveredCell));
+
+        return () => {
+
+        }
+    }, [isObjectChanging, hoveredCell]);
+
+
+    return onGameObjectPressed;
 }
