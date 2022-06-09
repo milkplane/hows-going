91d64ac229a25d2baf1e66dd70806216d895957c
milkplane <victorgelign@gmail.com>
2022-06-09 18:19:25 +0300

    Extract Options Panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import Map from './features/finding/Map';
 import OptionsPanel from './features/finding/OptionsPanel';
-import { Col, Layout, Row } from 'antd';
-const Sider = Layout.Sider;
+import { Col, Row, Space } from 'antd';
+import MapChanger from './features/finding/MapChanger';
+import GreedSlider from './features/finding/GreedSlider';
+import ToolSelect from './features/finding/ToolSelect';
+import SearchButton from './features/finding/SearchButton';
+import styled from 'styled-components';
+import PathfinderInfo from './features/finding/PathfinderInfo';
+
+const AppName = styled.h1`
+    font-size: 36px;
+    font-family: sans-serif;
+`
 
 function App() {
   return (
     <Row align='middle'>
       <Col span={24} lg={9}>
-        <Row align='middle' justify='center' style={{height: '100vh'}}>
-          <OptionsPanel />
+        <Row align='middle' justify='center' style={{ height: '100vh' }}>
+          <OptionsPanel>
+            <AppName>How's it going</AppName>
+            <MapChanger />
+            <Space direction="vertical" style={{ width: "80%" }}>
+              <p>Жадность поиска</p>
+              <GreedSlider />
+            </Space>
+            <PathfinderInfo/>
+            <ToolSelect/>
+            <SearchButton />
+          </OptionsPanel>
         </Row>
       </Col>
       <Col span={24} lg={15}>
--- src/features/finding/OptionsPanel.tsx
+++ src/features/finding/OptionsPanel.tsx
@@ -1,41 +1,23 @@
-import PathfinderInfo from "./PathfinderInfo";
-import SearchButton from "./SearchButton";
-import ToolSelect from "./ToolSelect";
 import { Col, Row, Space } from 'antd';
-import styled from "styled-components";
-import GreedSlider from "./GreedSlider";
-import MapChanger from "./MapChanger";
+import { Children } from 'react';
+import { ReactNode } from "react";
 
-const AppName = styled.h1`
-    font-size: 36px;
-    font-family: sans-serif;
-`
+type OptionsPanelProps = {
+    children?: ReactNode
+}
 
-const OptionsPanel = () => {
+const OptionsPanel = (props: OptionsPanelProps) => {
+    const rows = Children.map(props.children,
+        (child) => {
+            return <Row justify="center">
+                {child}
+            </Row>
+        });
+
     return <Row justify="center" align="middle">
         <Col span={16} style={{ justifyContent: 'center' }}>
             <Space direction="vertical" size={50}>
-                <Row justify="center">
-                    <AppName>How's it going</AppName>
-                </Row>
-                <Row justify="center">
-                    <MapChanger/>
-                </Row>
-                <Row justify="center">
-                    <Space direction="vertical" style={{ width: "80%" }}>
-                        <p>Жадность поиска</p>
-                        <GreedSlider/>
-                    </Space>
-                </Row>
-                <Row justify="center">
-                    <PathfinderInfo />
-                </Row>
-                <Row justify="center">
-                    <ToolSelect />
-                </Row>
-                <Row justify="center">
-                    <SearchButton />
-                </Row>
+                {rows}
             </Space>
         </Col>
     </Row>
