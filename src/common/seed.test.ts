1a69fe0074e82e4b474d4e6e6f4be36cf1d6b969
milkplane <victorgelign@gmail.com>
2022-06-27 18:14:22 +0300

    Add getting number from string seed

--- src/common/seed.test.ts
+++ src/common/seed.test.ts
@@ -1,4 +1,4 @@
-import { createSeed } from "./seed";
+import { createSeed, getNumberFromSeed, getUnitFromSeed } from "./seed";
 
 describe("seed", () => {
     it("should generate one word", () => {
@@ -17,7 +17,76 @@ describe("seed", () => {
         }
 
         const uniqueWords = new Set(words);
-        
+
         expect(uniqueWords.size).not.toBe(seedNumber);
     });
+
+    it("should generate number from seed with collision up to 3%", () => {
+        const letters = `ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        abcdefghijklmnopqrstuvwxyz
+        АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ
+        абвгдеёжзийклмнопрстуфхцчшщъыьэюя`
+
+        const words = [];
+
+        for (let i = 0; i < 100000; i++) {
+            const length = Math.floor(Math.random() * 100);
+            let word = "";
+
+            for (let j = 0; j < length; j++) {
+                word += letters[Math.floor(Math.random() * letters.length)];
+            }
+
+            words.push(word);
+        }
+
+        const numbers = words.map(word => getNumberFromSeed(word));
+        const uniqueNumbers = new Set(numbers).size;
+        const uniqueRatio = uniqueNumbers / words.length;
+
+        expect(uniqueRatio).toBeGreaterThan(0.97);
+    })
+
+    it("should generate integer", () => {
+        const words = [];
+
+        for (let i = 0; i < 1000; i++) {
+            words.push(createSeed());
+        }
+
+        const numbers = words.map(word => getNumberFromSeed(word));
+        numbers.forEach(number => {
+            expect(Math.round(number)).toBe(number);
+        })
+
+    })
+
+    it("should generate number from seed less or equal than 32bit number", () => {
+        const words = [];
+
+        for (let i = 0; i < 1000; i++) {
+            words.push(createSeed());
+        }
+
+        const numbers = words.map(word => getNumberFromSeed(word));
+        numbers.forEach(number => {
+            expect(Math.abs(number)).toBeLessThanOrEqual(4294967295);
+        })
+
+    })
+
+    it("should generate unit number", () => {
+        const words = [];
+
+        for (let i = 0; i < 1000; i++) {
+            words.push(createSeed());
+        }
+
+        const numbers = words.map(word => getUnitFromSeed(word));
+        numbers.forEach(number => {
+            expect(number).toBeLessThanOrEqual(1);
+            expect(number).toBeGreaterThanOrEqual(-1);
+        })
+
+    })
 })
--- src/common/seed.ts
+++ src/common/seed.ts
@@ -5,3 +5,19 @@ export type Seed = string;
 export const createSeed = (): Seed => {
     return randomWords(1)[0];
 }
+
+export const getNumberFromSeed = (seed: Seed): number => {
+    if (seed.length === 0) return 0;
+
+    let number = seed.charCodeAt(0);
+
+    for (let i = 1; i < seed.length; i++) {
+        number = (number * Math.pow(seed[i].charCodeAt(0), i)) % 4294967295;
+    }
+
+    return number;
+}
+
+export const getUnitFromSeed = (seed: Seed): number => {
+    return getNumberFromSeed(seed) / 4294967295;
+}
